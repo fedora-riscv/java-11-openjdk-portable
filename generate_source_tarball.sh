34397838df73cb81297c29df171c6bbbd84fcfaf
Andrew John Hughes <gnu_andrew@member.fsf.org>
2023-04-27 07:36:47 +0100

    Update to jdk-11.0.19.0+7
    Update release notes to 11.0.19.0+7
    Require tzdata 2023c due to local inclusion of JDK-8274864 & JDK-8305113
    Update generate_tarball.sh to add support for passing a boot JDK to the configure run
    Add POSIX-friendly error codes to generate_tarball.sh and fix whitespace
    Remove .jcheck and GitHub support when generating tarballs, as done in upstream release tarballs
    Rebase FIPS support against 11.0.19+6
    Rebase RH1750419 alt-java patch against 11.0.19+6
    Update TestTranslations.java to test the new America/Ciudad_Juarez zone

--- generate_source_tarball.sh
+++ generate_source_tarball.sh
@@ -17,7 +17,7 @@
 # PROJECT_NAME=release
 # OPENJDK_URL=http://icedtea.classpath.org/hg/
 # TO_COMPRESS="*/tapset"
-# 
+#
 # They are used to create correct name and are used in construction of sources url (unless REPO_ROOT is set)
 
 # This script creates a single source tarball out of the repository
@@ -55,30 +55,59 @@ if [ "x$1" = "xhelp" ] ; then
     echo "OPENJDK_URL - the URL to retrieve code from (optional; defaults to ${OPENJDK_URL_DEFAULT})"
     echo "COMPRESSION - the compression type to use (optional; defaults to ${COMPRESSION_DEFAULT})"
     echo "FILE_NAME_ROOT - name of the archive, minus extensions (optional; defaults to PROJECT_NAME-REPO_NAME-VERSION)"
-    echo "REPO_ROOT - the location of the Mercurial repository to archive (optional; defaults to OPENJDK_URL/PROJECT_NAME/REPO_NAME)"
+    echo "REPO_ROOT - the location of the Git repository to archive (optional; defaults to OPENJDK_URL/PROJECT_NAME/REPO_NAME)"
     echo "TO_COMPRESS - what part of clone to pack (default is openjdk)"
     echo "GH001 - the path to the ECC code patch, GH001, to apply (optional; downloaded if unavailable)"
     echo "GH003 - the path to the ECC test patch, GH003, to apply (optional; downloaded if unavailable)"
+    echo "BOOT_JDK - the bootstrap JDK to satisfy the configure run"
     exit 1;
 fi
 
 
 if [ "x$VERSION" = "x" ] ; then
     echo "No VERSION specified"
-    exit -2
+    exit 2
 fi
 echo "Version: ${VERSION}"
-    
+
+NUM_VER=${VERSION##jdk-}
+RELEASE_VER=${NUM_VER%%+*}
+BUILD_VER=${NUM_VER##*+}
+MAJOR_VER=${RELEASE_VER%%.*}
+echo "Major version is ${MAJOR_VER}, release ${RELEASE_VER}, build ${BUILD_VER}"
+
+if [ "x$BOOT_JDK" = "x" ] ; then
+    echo "No boot JDK specified".
+    BOOT_JDK=/usr/lib/jvm/java-${MAJOR_VER}-openjdk;
+    echo -n "Checking for ${BOOT_JDK}...";
+    if [ -d ${BOOT_JDK} -a -x ${BOOT_JDK}/bin/java ] ; then
+        echo "Boot JDK found at ${BOOT_JDK}";
+    else
+        echo "Not found";
+        PREV_VER=$((${MAJOR_VER} - 1));
+        BOOT_JDK=/usr/lib/jvm/java-${PREV_VER}-openjdk;
+        echo -n "Checking for ${BOOT_JDK}...";
+        if [ -d ${BOOT_JDK} -a -x ${BOOT_JDK}/bin/java ] ; then
+            echo "Boot JDK found at ${BOOT_JDK}";
+        else
+            echo "Not found";
+            exit 4;
+        fi
+    fi
+else
+    echo "Boot JDK: ${BOOT_JDK}";
+fi
+
 # REPO_NAME is only needed when we default on REPO_ROOT and FILE_NAME_ROOT
 if [ "x$FILE_NAME_ROOT" = "x" -o "x$REPO_ROOT" = "x" ] ; then
   if [ "x$PROJECT_NAME" = "x" ] ; then
     echo "No PROJECT_NAME specified"
-    exit -1
+    exit 1
   fi
   echo "Project name: ${PROJECT_NAME}"
   if [ "x$REPO_NAME" = "x" ] ; then
     echo "No REPO_NAME specified"
-    exit -3
+    exit 3
   fi
   echo "Repository name: ${REPO_NAME}"
 fi
@@ -104,9 +133,10 @@ if [ "x$REPO_ROOT" = "x" ] ; then
     REPO_ROOT="${OPENJDK_URL}/${PROJECT_NAME}/${REPO_NAME}.git"
     echo "No repository root specified; default to ${REPO_ROOT}"
 fi;
+
 if [ "x$TO_COMPRESS" = "x" ] ; then
     TO_COMPRESS="openjdk"
-    echo "No to be compressed targets specified, ; default to ${TO_COMPRESS}"
+    echo "No targets to be compressed specified, ; default to ${TO_COMPRESS}"
 fi;
 
 echo -e "Settings:"
@@ -120,6 +150,7 @@ echo -e "\tREPO_ROOT: ${REPO_ROOT}"
 echo -e "\tTO_COMPRESS: ${TO_COMPRESS}"
 echo -e "\tGH001: ${GH001}"
 echo -e "\tGH003: ${GH003}"
+echo -e "\tBOOT_JDK: ${BOOT_JDK}"
 
 if [ -d ${FILE_NAME_ROOT} ] ; then
   echo "exists exists exists exists exists exists exists "
@@ -142,14 +173,14 @@ pushd "${FILE_NAME_ROOT}"
         pushd openjdk
             echo "Removing EC source code we don't build"
             CRYPTO_PATH=src/jdk.crypto.ec/share/native/libsunec/impl
-	    rm -vf ${CRYPTO_PATH}/ec2.h
-	    rm -vf ${CRYPTO_PATH}/ec2_163.c
-	    rm -vf ${CRYPTO_PATH}/ec2_193.c
-	    rm -vf ${CRYPTO_PATH}/ec2_233.c
-	    rm -vf ${CRYPTO_PATH}/ec2_aff.c
-	    rm -vf ${CRYPTO_PATH}/ec2_mont.c
-	    rm -vf ${CRYPTO_PATH}/ecp_192.c
-	    rm -vf ${CRYPTO_PATH}/ecp_224.c
+            rm -vf ${CRYPTO_PATH}/ec2.h
+            rm -vf ${CRYPTO_PATH}/ec2_163.c
+            rm -vf ${CRYPTO_PATH}/ec2_193.c
+            rm -vf ${CRYPTO_PATH}/ec2_233.c
+            rm -vf ${CRYPTO_PATH}/ec2_aff.c
+            rm -vf ${CRYPTO_PATH}/ec2_mont.c
+            rm -vf ${CRYPTO_PATH}/ecp_192.c
+            rm -vf ${CRYPTO_PATH}/ecp_224.c
 
             echo "Syncing EC list with NSS"
             if [ "x$GH001" = "x" ] ; then
@@ -182,10 +213,28 @@ pushd "${FILE_NAME_ROOT}"
     # Generate .src-rev so build has knowledge of the revision the tarball was created from
     mkdir build
     pushd build
-    sh ${PWD}/../openjdk/configure
+    sh ${PWD}/../openjdk/configure --with-boot-jdk=${BOOT_JDK}
     make store-source-revision
     popd
     rm -rf build
+
+    # Remove commit checks
+    echo "Removing $(find openjdk -name '.jcheck' -print)"
+    find openjdk -name '.jcheck' -print0 | xargs -0 rm -r
+
+    # Remove history and GHA
+    echo "find openjdk -name '.hgtags'"
+    find openjdk -name '.hgtags' -exec rm -v '{}' '+'
+    echo "find openjdk -name '.hgignore'"
+    find openjdk -name '.hgignore' -exec rm -v '{}' '+'
+    echo "find openjdk -name '.gitattributes'"
+    find openjdk -name '.gitattributes' -exec rm -v '{}' '+'
+    echo "find openjdk -name '.gitignore'"
+    find openjdk -name '.gitignore' -exec rm -v '{}' '+'
+    echo "find openjdk -name '.git'"
+    find openjdk -name '.git' -exec rm -rv '{}' '+'
+    echo "find openjdk -name '.github'"
+    find openjdk -name '.github' -exec rm -rv '{}' '+'
 
     echo "Compressing remaining forest"
     if [ "X$COMPRESSION" = "Xxz" ] ; then
